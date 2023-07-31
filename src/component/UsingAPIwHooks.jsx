578ce41d2c3780e82fd875e0ac0f5b763bee412a
Deepak Chandra MAurya <44018172+deepakchandra4@users.noreply.github.com>
2023-07-31 18:03:37 +0530

    ..

--- src/component/UsingAPIwHooks.jsx
+++ src/component/UsingAPIwHooks.jsx
@@ -1,20 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const UsingAPIwHooks = () => {
-    const API = {
-        key : "f4d24ed4eed3f437a172c7d21502e1b4",
-        base : "https://openweathermap.org/data/"
-    }
-    const [temp , setTemp] = useState([])
+  // const url = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f4d24ed4eed3f437a172c7d21502e1b4"
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        axios
-    })
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(url);
+      const jresult = await result.json();
+      setItems(jresult)
+    };
+    fetchData();
+  }, []);
   return (
     <div>
-      
+        {
+            items.map((item ) => {
+                return <li key={item}>Name : {item.name} | Email: {item.email}</li>
+            })
+        }
     </div>
-  )
-}
+  );
+};
 
-export default UsingAPIwHooks
+export default UsingAPIwHooks;
